412e15391ea4dbdea0a654d72c3158980f98cc20
haylibi <duarte.pinto7337@gmail.com>
2025-07-06 19:20:18 +0200

    changed import to include "src"

--- hack_tokenizer/src/benchmark/__init__.py
+++ hack_tokenizer/src/benchmark/__init__.py
@@ -9,7 +9,7 @@ for benchmark in os.listdir((pathlib.Path(__file__) / '..').resolve()):
     if benchmark in ['__init__.py', 'base.py'] or not benchmark.endswith('.py'): continue
     # Import the benchmark
     benchmark = benchmark.replace('.py', '')
-    bench = importlib.import_module(f'hack_tokenizer.benchmark.{benchmark}')
+    bench = importlib.import_module(f'hack_tokenizer.src.benchmark.{benchmark}')
     BENCHMARKS.append(eval(f'bench.{benchmark}()'))
 BENCHMARKS = Benchmarks(BENCHMARKS)
 

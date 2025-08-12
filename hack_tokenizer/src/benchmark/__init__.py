6d55d93cafb57cf2b60851b1362628a98efc2085
yalilont <duarte.pinto7337@gmail.com>
2025-08-12 10:52:32 +0200

    reverted back to include all benchmarks

--- hack_tokenizer/src/benchmark/__init__.py
+++ hack_tokenizer/src/benchmark/__init__.py
@@ -1,16 +1,14 @@
 from .base import Benchmarks
-# import os
-# import pathlib
-# import importlib
+import os
+import pathlib
+import importlib
 
-# BENCHMARKS = []
-# for benchmark in os.listdir((pathlib.Path(__file__) / '..').resolve()):
-#     if benchmark in ['__init__.py', 'base.py'] or not benchmark.endswith('.py'): continue
-#     # Import the benchmark
-#     benchmark = benchmark.replace('.py', '')
-#     bench = importlib.import_module(f'hack_tokenizer.src.benchmark.{benchmark}')
-#     BENCHMARKS.append(eval(f'bench.{benchmark}()'))
-# BENCHMARKS = Benchmarks(BENCHMARKS)
-from .MMLU import MMLU
-BENCHMARKS = Benchmarks([MMLU()])
+BENCHMARKS = []
+for benchmark in os.listdir((pathlib.Path(__file__) / '..').resolve()):
+    if benchmark in ['__init__.py', 'base.py'] or not benchmark.endswith('.py'): continue
+    # Import the benchmark
+    benchmark = benchmark.replace('.py', '')
+    bench = importlib.import_module(f'hack_tokenizer.src.benchmark.{benchmark}')
+    BENCHMARKS.append(eval(f'bench.{benchmark}()'))
+BENCHMARKS = Benchmarks(BENCHMARKS)
 __all__ = ['BENCHMARKS']

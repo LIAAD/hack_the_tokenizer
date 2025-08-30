6a2dd14ddf6b880cd7fc642a8e7cfe18213897b0
yalilont <duarte.pinto7337@gmail.com>
2025-08-30 19:43:49 +0200

    Added tasks "COPA" and "multirc"

--- src/hack_tokenizer/benchmark/SupergluePTPT.py
+++ src/hack_tokenizer/benchmark/SupergluePTPT.py
@@ -36,6 +36,65 @@ def task_boolq(
     }
 
 
+def task_multirc(
+    benchmark: Benchmark,
+    predictions: dict[str, Any]
+):
+    dataset = benchmark.df.to_dict('records')
+
+    benchmark_output: list[dict[str, str]] = []
+    # After obtaining predictions start the evaluation process
+    for data, prediction in zip(dataset, predictions['generated_text']):
+        # if len(prediction) > 1: prediction = prediction[0]
+        # Retrieve the actual answer from the prediction
+        predicted_answer = functions.get_first_word(data['prediction_prompts'], prediction)
+        if not predicted_answer in ['1', '0']:
+            if predicted_answer is None:
+                predicted_answer = '-1'
+            elif predicted_answer.lower().startswith('sim') or predicted_answer.lower().startswith('verda'):
+                predicted_answer = '1'
+            elif predicted_answer.lower().startswith('não') or predicted_answer.lower().startswith('mentir'):
+                predicted_answer = '0'
+        benchmark_output.append({
+            'idx': data['idx'], 'input_text': data['prediction_prompts'], 'prediction_text': prediction, 'prediction_label': predicted_answer, 'correct_label': data['label']
+        })
+    accurate_preds = sum(ben['prediction_label'].strip()[:1] == str(ben['correct_label']) for ben in benchmark_output)
+    return {
+        'benchmark': 'Superglue pt-PT: Task MultiRC',
+        'accuracy': accurate_preds / len(dataset),
+        'accurate_predictions': accurate_preds,
+        'wrong_predictions': len(dataset) - accurate_preds,
+        'benchmark_predictions': benchmark_output,
+    }
+
+
+def task_copa(
+    benchmark: Benchmark,
+    predictions: dict[str, Any]
+):
+    dataset = benchmark.df.to_dict('records')
+
+    benchmark_output: list[dict[str, str]] = []
+    # After obtaining predictions start the evaluation process
+    for data, prediction in zip(dataset, predictions['generated_text']):
+        # if len(prediction) > 1: prediction = prediction[0]
+        # Retrieve the actual answer from the prediction
+        predicted_answer = functions.get_first_word(data['prediction_prompts'], prediction)
+        if not predicted_answer in ['1', '0']:
+            if predicted_answer is None:
+                predicted_answer = '-1'
+        benchmark_output.append({
+            'idx': data['idx'], 'input_text': data['prediction_prompts'], 'prediction_text': prediction, 'prediction_label': predicted_answer, 'correct_label': data['label']
+        })
+    accurate_preds = sum(ben['prediction_label'].strip()[:1] == str(ben['correct_label']) for ben in benchmark_output)
+    return {
+        'benchmark': 'Superglue pt-PT: Task COPA',
+        'accuracy': accurate_preds / len(dataset),
+        'accurate_predictions': accurate_preds,
+        'wrong_predictions': len(dataset) - accurate_preds,
+        'benchmark_predictions': benchmark_output,
+    }
+
 class SupergluePTPT(Benchmark):
     def __init__(self):
         # Loading CALAME-PT dataset onto a Pandas DataFrame
